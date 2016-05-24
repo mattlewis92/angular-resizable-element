86fbe6ff24627536be788388e7218bec61629ac1
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-05-24 18:59:05 +0100

    chore: create basic demo app and directive

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,9 +1,24 @@
 import {Component} from '@angular/core';
-import {HelloWorld} from './../angular2-resizable';
+import {Resizable} from './../angular2-resizable';
 
 @Component({
   selector: 'demo-app',
-  directives: [HelloWorld],
-  template: '<hello-world></hello-world>'
+  directives: [Resizable],
+  styles: [`
+    .rectangle {
+      position: relative;
+      top: 100px;
+      width: 300px;
+      height: 150px;
+      background-color: red;
+      border: solid 1px black;
+      margin: auto;
+    }
+  `],
+  template: `
+    <div class="text-center">
+      <div class="rectangle" mwl-resizeable></div>
+    </div>
+  `
 })
 export class DemoApp {}
---
+++ src/resizable.directive.ts
@@ -0,0 +1,8 @@
+import {
+  Directive
+} from '@angular/core';
+
+@Directive({
+  selector: '[mwl-resizeable]'
+})
+export class Resizable {}
