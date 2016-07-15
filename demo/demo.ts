0a5bf113c15c8c0eeb1fca764f9db989ca28c91b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-07-15 16:52:51 +0100

    fix(demo): fix broken demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -35,7 +35,7 @@ import {Resizable, ResizeEvent, ResizeHandle} from './../angular2-resizable';
         mwl-resizable
         [validateResize]="validate"
         [resizeEdges]="{bottom: true, right: true, top: true, left: true}"
-        [enableResizeStyling]="true"
+        [enableGhostResize]="true"
         [resizeSnapGrid]="{left: 50, right: 50}"
         (onResizeEnd)="onResizeEnd($event)">
         <img
