d5b18cc67915b38890d3c24b7aa63db32e77136f
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-09-03 14:01:09 +0100

    feat(angular): support RC6

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
     files: [
--- test/entry.ts
+++ test/entry.ts
@@ -3,6 +3,10 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/proxy';
+import 'zone.js/dist/jasmine-patch';
 import 'rxjs';
 import {
   TestBed
