8de33c8e56492ca18fba62d42e58a1fae176f66d
Matt Lewis <mattlewis92@users.noreply.github.com>
2017-03-16 18:01:41 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './resizable.module';
 export {ResizeEvent} from './interfaces/resizeEvent.interface'; // tslint:disable-line
 export {Resizable} from './resizable.directive'; // tslint:disable-line
+export {ResizeHandle} from './resizeHandle.directive'; // tslint:disable-line
