e8ee708327a372bd4ff0dd80b3b976e0ea03ac77
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-03-23 09:55:03 +0000

    chore: use webpack and rxjs externals

--- webpack.config.umd.ts
+++ webpack.config.umd.ts
@@ -1,5 +1,8 @@
 import * as webpack from 'webpack';
 
+const angularExternals = require('webpack-angular-externals');
+const rxjsExternals = require('webpack-rxjs-externals');
+
 export default {
   entry: __dirname + '/src/index.ts',
   output: {
@@ -8,74 +11,10 @@ export default {
     libraryTarget: 'umd',
     library: 'angularResizableElement'
   },
-  externals: {
-    '@angular/core': {
-      root: ['ng', 'core'],
-      commonjs: '@angular/core',
-      commonjs2: '@angular/core',
-      amd: '@angular/core'
-    },
-    '@angular/common': {
-      root: ['ng', 'common'],
-      commonjs: '@angular/common',
-      commonjs2: '@angular/common',
-      amd: '@angular/common'
-    },
-    'rxjs/Subject': {
-      root: ['rx', 'Subject'],
-      commonjs: 'rxjs/Subject',
-      commonjs2: 'rxjs/Subject',
-      amd: 'rxjs/Subject'
-    },
-    'rxjs/Observable': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/Observable',
-      commonjs2: 'rxjs/Observable',
-      amd: 'rxjs/Observable'
-    },
-    'rxjs/observable/merge': {
-      root: ['rx', 'Observable', 'merge'],
-      commonjs: 'rxjs/observable/merge',
-      commonjs2: 'rxjs/observable/merge',
-      amd: 'rxjs/observable/merge'
-    },
-    'rxjs/add/operator/map': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/map',
-      commonjs2: 'rxjs/add/operator/map',
-      amd: 'rxjs/add/operator/map'
-    },
-    'rxjs/add/operator/mergeMap': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/mergeMap',
-      commonjs2: 'rxjs/add/operator/mergeMap',
-      amd: 'rxjs/add/operator/mergeMap'
-    },
-    'rxjs/add/operator/takeUntil': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/takeUntil',
-      commonjs2: 'rxjs/add/operator/takeUntil',
-      amd: 'rxjs/add/operator/takeUntil'
-    },
-    'rxjs/add/operator/filter': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/filter',
-      commonjs2: 'rxjs/add/operator/filter',
-      amd: 'rxjs/add/operator/filter'
-    },
-    'rxjs/add/operator/pairwise': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/pairwise',
-      commonjs2: 'rxjs/add/operator/pairwise',
-      amd: 'rxjs/add/operator/pairwise'
-    },
-    'rxjs/add/operator/take': {
-      root: ['rx', 'Observable'],
-      commonjs: 'rxjs/add/operator/take',
-      commonjs2: 'rxjs/add/operator/take',
-      amd: 'rxjs/add/operator/take'
-    }
-  },
+  externals: [
+    angularExternals(),
+    rxjsExternals()
+  ],
   devtool: 'source-map',
   module: {
     rules: [{
