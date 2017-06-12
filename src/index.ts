10051cd8a04efa002ed357dbf04c529b56822a15
Chris McKnight <cmckni3@gmail.com>
2017-06-12 10:59:55 -0500

    feat: export Edges and BoundingRectangle interfaces for use within applications

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './resizable.module';
+export {BoundingRectangle} from './interfaces/boundingRectangle.interface'; // tslint:disable-line
+export {Edges} from './interfaces/edges.interface'; // tslint:disable-line
 export {ResizeEvent} from './interfaces/resizeEvent.interface'; // tslint:disable-line
 export {Resizable} from './resizable.directive'; // tslint:disable-line
 export {ResizeHandle} from './resizeHandle.directive'; // tslint:disable-line
