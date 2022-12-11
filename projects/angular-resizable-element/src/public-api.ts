767bd1bb00c85651fcd80449e191fa75cd056621
Matt Lewis <hello@mattlewis.me>
2022-12-11 13:48:11 +0000

    fix: expose ResizeCursors interface as part of the public api

--- projects/angular-resizable-element/src/public-api.ts
+++ projects/angular-resizable-element/src/public-api.ts
@@ -6,5 +6,5 @@ export * from './lib/resizable.module';
 export { BoundingRectangle } from './lib/interfaces/bounding-rectangle.interface';
 export { Edges } from './lib/interfaces/edges.interface';
 export { ResizeEvent } from './lib/interfaces/resize-event.interface';
-export { ResizableDirective } from './lib/resizable.directive';
+export { ResizableDirective, ResizeCursors } from './lib/resizable.directive';
 export { ResizeHandleDirective } from './lib/resize-handle.directive';
