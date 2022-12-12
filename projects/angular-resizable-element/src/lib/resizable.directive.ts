d72fb145c1c45b8ccbdaa63bd43bce9fa1de260e
Dan Hobbs <danhobbs75@gmail.com>
2022-12-12 07:33:15 +0000

    fix: check if event is cancelable

--- projects/angular-resizable-element/src/lib/resizable.directive.ts
+++ projects/angular-resizable-element/src/lib/resizable.directive.ts
@@ -323,7 +323,9 @@ export class ResizableDirective implements OnInit, OnDestroy {
       tap(({ event }) => {
         if (currentResize) {
           try {
-            event.preventDefault();
+            if (event.cancelable) {
+              event.preventDefault();
+            }
           } catch (e) {
             // just adding try-catch not to see errors in console if there is a passive listener for same event somewhere
             // browser does nothing except of writing errors to console
